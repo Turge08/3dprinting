b7761f3aa0037d9a3c0821244edf17f8790d171e
Turgeon <steve.turgeon@sterbc.com>
2021-02-07 17:22:30 -0500

    ...

--- webcam/install.sh
+++ webcam/install.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+clear
+
 sudo apt-get install build-essential libjpeg8-dev imagemagick libv4l-dev cmake git -y
 mkdir ~/mjpg-streamer
 cd ~/mjpg-streamer
@@ -7,4 +11,6 @@ cd mjpg-streamer/mjpg-streamer-experimental
 
 make
 sudo make install
+
+wget https://raw.githubusercontent.com/Turge08/3dprinting/master/webcam/stream.sh 
 
